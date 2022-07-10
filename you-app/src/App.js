19f4e9046be37c0274fdcd0ce93cee8576b33eaf
sados45 <sados45@gmail.com>
2022-07-11 00:16:41 +0200

    events, onClick, onChange, e.target.value, react-9

--- you-app/src/App.js
+++ you-app/src/App.js
@@ -94,65 +94,124 @@
 
 // YERINI BULMA________ NAVIGASYON ILE___________
 
-  import React, { Component } from 'react'
+  // import React, { Component } from 'react'
   
-  export default class App extends Component {
+  // export default class App extends Component {
 
-    constructor(){
+  //   constructor(){
 
-      super()
+  //     super()
 
-      this.state ={
+  //     this.state ={
 
-        latitude: '',
-        message: ''
-      }
+  //       latitude: '',
+  //       message: ''
+  //     }
      
       
-        }
+  //       }
 
-        // biz acilma islemi bittikten sonra bu navigasyon isleminin calismasini istiyorsak. componentDidMount yapmak zorundayiz. 
+  //       // biz acilma islemi bittikten sonra bu navigasyon isleminin calismasini istiyorsak. componentDidMount yapmak zorundayiz. 
 
-        componentDidMount(){
-          window.navigator.geolocation.getCurrentPosition(
+  //       componentDidMount(){
+  //         window.navigator.geolocation.getCurrentPosition(
 
-            position => this.setState({latitude:position.coords.latitude}),
-            error => {
-              setTimeout( () =>{
-               this.setState({message:error.message}) 
-              },1000)
+  //           position => this.setState({latitude:position.coords.latitude}),
+  //           error => {
+  //             setTimeout( () =>{
+  //              this.setState({message:error.message}) 
+  //             },1000)
               
-        }
-          )
-        } //normalde error => this.setState({message:error.message}) bu yazili idi ama timeout koyarak error, loading yazmasini navigasyon kapatarak döngü icerisinde takip edecegi islem sirasini görmek istedik. basarili oldu. 
+  //       }
+  //         )
+  //       } //normalde error => this.setState({message:error.message}) bu yazili idi ama timeout koyarak error, loading yazmasini navigasyon kapatarak döngü icerisinde takip edecegi islem sirasini görmek istedik. basarili oldu. 
 
-        componentDidUpdate() {
+  //       componentDidUpdate() {
 
-          console.log('componentDidUpdate') //asagida latitude ve error bilgilerini de girdikten sonra bilgi update oldugu icin console ile bunu görmek istedik  ve bilginin dogru oldugunu teyit ettik. 
-        }
+  //         console.log('componentDidUpdate') //asagida latitude ve error bilgilerini de girdikten sonra bilgi update oldugu icin console ile bunu görmek istedik  ve bilginin dogru oldugunu teyit ettik. 
+  //       }
 
-    render() {
-      // return (
+  //   render() {
+  //     // return (
 
-        {
+  //       {
 
-          if(this.state.latitude && ! this.state.message){
+  //         if(this.state.latitude && ! this.state.message){
 
-            return <h1>Latitude: {this.state.latitude} </h1>
-          }
+  //           return <h1>Latitude: {this.state.latitude} </h1>
+  //         }
 
-          if(!this.state.latitude && ! this.state.message){
-            return <h1>Error Message: {this.state.message}</h1>
+  //         if(!this.state.latitude && ! this.state.message){
+  //           return <h1>Error Message: {this.state.message}</h1>
 
-          }
-          return <h1>Loading...</h1>
-        }
-        // <div>
+  //         }
+  //         return <h1>Loading...</h1>
+  //       }
+  //       // <div>
 
-        //   <h1> Latitude:{this.state.latitude} </h1>
-        //   <h1> Error: {this.state.message} </h1>
-        // </div> 
-      // )
-    }
-  }
+  //       //   <h1> Latitude:{this.state.latitude} </h1>
+  //       //   <h1> Error: {this.state.message} </h1>
+  //       // </div> 
+  //     // )
+  //   }
+  // }
   
+
+  //REACT EVENT _________________________-.Ders React -9:
+
+
+  import React, { Component } from 'react'
+  import "./App.css"
+  
+  export default class App extends Component {
+    
+
+    state={
+      value: ''
+     
+    }
+
+
+    handleClick=() =>{
+      console.log('this element is clicked');
+    }
+    handleLabelClick =() =>{
+      console.log('label clicked');
+      console.log('input changed');
+
+    handleInputChange =(event) => { //icinde event bircok özelligi barindirir. 
+
+      // console.log('event.target.value') // ÖNEMLI bu sekilde yazarak input icersinde yaptigim en kucuk degisiklik consolda görülmekte. 
+      this.setState({value:e.target.value}) //biz burada state icinde value acarak ve asagida h1 icinde value ile ilgili cagirarak, aslinda input icersinde yazilan degeri yakalayip bunu arama yapmayi ileride hedefleyecegiz. star wars kelimesini yakala mesela, bunula ilgili filmleri getirmek icin kullancagiz. 
+    }
+
+    render () {
+
+      return (
+        <div className='input-card'>
+        <form>
+
+          <label onClick={this.handleLabelClick}>Enter a search term</label> 
+          <br/>
+          <input 
+          onClick={this.handleClick}
+          style = {{width:'50%', height:'60%'}} 
+          // onChange ={this.handleInputChange} //bir diger yöntem üstte yazili.
+          // onChange= {() => console.log('input changed')} //bu sekilde kisaca yazilabilir. cleankod...
+          // onChange={(event) => console.log(event.target.value)}// eventleri sadece e harfi yazarak da gösterebiliriz.
+          onChange={this.handleInputChange}
+          type='text'
+          value= {this.state.value} //biz input icersine value deger girmemistik kontrol tipi inoutta bu sekilde deger giriyoruz. 
+          />
+                    
+        </form>
+          {/* <button onClick= {this.handleClick}>Submit</button> */}
+          {/* <h1> Value: {this.state.value}</h1>  */}
+        </div>
+      )
+    }
+  
+  } //burada onClick tanimlandiktan sonra istedigimiz her yere onClik tanimlamasi yapip köseli parantez icerisinde aciklamasiyla aktif hale getirebiliyoruz. 
+  }
+
+  //inputlar iki türlu kontrol ve uncontrol diye ayriliyor. biz unkontrolu görduk. 
---
+++ you-app/src/react-user-event/event.js
@@ -0,0 +1,7 @@
+//EVENT KONUSU_______________-:
+
+// User clıcks on somethıng ..... onClick  ---her yerde calisir. bir sayfa üzerinde istedigim heryerde kullanbilirim. 
+
+// User changes text in an input.....onChange--- inputlarda yaptigimiz her degisiklikten sonra calisan bir fonksiyondur.sadece inputlarda kullanabilirim.
+
+// User submits a form ...... onSubmit ---bunu sadece formlarda kullanabilirim.
